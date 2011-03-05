d3032841bc403d90c79589c4f143657fcb07e21b
Juan Carlos Quiroz <juan.carlos.quiroz.ramos@gmail.com>
2011-03-04 21:36:21 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inventory::Application.config.secret_token = '9ef7eb0f07892fee25306844210611106a4a28f794dde70bf108d72c331498c0c4f3735b04c099d108d7203ff9167878402f6239809e79abbb4571cd14ec85b6'
